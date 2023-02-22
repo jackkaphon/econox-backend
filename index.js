0070b84fa7cddbf748a4ba2240cd94c94ff16c88
jackkaphon <jackwchh@gmail.com>
2023-02-22 16:23:45 +0700

    add cookie

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+var cookieParser = require('cookie-parser')
 
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'admin',
-  password: 'password',
-  database: 'jack'
+    host: '127.0.0.1',
+    user: 'admin',
+    password: 'password',
+    database: 'jack'
 })
 
 // connection.connect()
 
-app.use(cors())
+app.use(cookieParser())
+app.use(cors({
+    // origin: 'http://localhost:5173',
+    origin: 'https://www.wannabedevs.com',
+    credentials: true
+}))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.cookie("user", 1)
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
